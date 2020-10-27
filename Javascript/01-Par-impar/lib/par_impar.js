10ff00ee0751718934ca72cda8c50572ba62401b
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-10-26 22:13:55 -0300

    cleaned

--- Javascript/01-Par-impar/lib/par_impar.js
+++ Javascript/01-Par-impar/lib/par_impar.js
@@ -1,5 +1,16 @@
 const parImpar = (number) => {
-  // Esta función debe devolver "par" si el número es par, "impar" si no lo es
+  let perro = new String;
+  if ( number % 2 === 0 ) { 
+    perro = 'par';
+  } else if (typeof(number) === "boolean") {
+    perro = 'no es un numero';
+  } else if (typeof(number) === "string") {
+    perro = 'no es un numero';
+  } else {
+    perro = 'impar';
+  };
+  console.log(typeof(number));
+  return perro;
 };
 
 module.exports = parImpar; // No remover
