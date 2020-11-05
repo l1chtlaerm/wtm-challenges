9f4b6004c75ac27c7438629ac16a99b5d5acd2d6
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-11-05 20:04:10 -0300

    corrected syntax

--- Javascript/12-Hamburguesas/test/test.js
+++ Javascript/12-Hamburguesas/test/test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const Combo = require("../lib/hamburguesas");
 
 describe("Combo", () => {
-  it("Las instancias de Combo deben tener una propiedad 'descripcion' que devuelva 'Hamburguesa' si no se pasa como parámetro", () => {
+  it("Las instancias de Combo deben tener una propiedad 'descripcion' que devuelva 'Hamburguesa' si no se pasa un parámetro", () => {
     assert.deepStrictEqual(new Combo().descripcion, "Hamburguesa");
   });
   it("Una instancia de Combo que haya recibido 'Coca Cola y Papas Grandes' debe incluirlo en descripcion", () => {
